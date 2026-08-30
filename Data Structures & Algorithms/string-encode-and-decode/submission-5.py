c6d8063135e07ede8f2ec38064ddc05e55f56daf
Srujanb2004 <srujanb2004@gmail.com>
2026-08-30 08:19:16 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        op=""
+        for s in strs:
+            op += str(len(s))+"#"+s
+        return op
+
+    def decode(self, s: str) -> List[str]:
+        op=[]
+        while s:
+            key = s.find("#")
+            index = int(s[:key])
+            op_string = s[key+1:index+key+1]
+            op.append(op_string)
+            s=s[key+index+1:]
+        return op
